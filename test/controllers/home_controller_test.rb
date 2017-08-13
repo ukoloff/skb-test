328dc9c88b7548e9430f77fbdd1ddc21613dd800
Stas Ukolov <ukoloff@gmail.com>
2017-08-13 16:21:23 +0500

    Тесты контроллеров

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_index
+    get '/'
+    assert_response :success
+  end
 end
